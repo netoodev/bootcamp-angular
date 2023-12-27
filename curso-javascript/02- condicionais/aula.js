6ef8857938335541613ee0189cba64e8d140bac1
Neto <netoodeveloper@gmail.com>
2023-12-27 16:34:41 -0300

    Estrutura Condicional com Else If

--- curso-javascript/02- condicionais/aula.js
+++ curso-javascript/02- condicionais/aula.js
@@ -1,8 +1,11 @@
-const numero = 0;
+const numero = 1;
 const numeroPar = (numero % 2) === 0;
 
-if (numeroPar) {
+if (numero === 0) {
+    console.log(`O número ${numero} é nulo!`);
+} else if (numeroPar) {
     console.log(`O número ${numero} é par!`);
 } else {
     console.log(`O número ${numero} é Ímpar!`);
 }
+
