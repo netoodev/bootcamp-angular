5214670f3b9a8decfb85ef14496f0ff889b90b8e
Neto <netoodeveloper@gmail.com>
2023-12-27 20:57:27 -0300

    Exemplo Prático com Funções

---
+++ curso-javascript/04 - funcoes/imc-com-funcoes.js
@@ -0,0 +1,28 @@
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura,2);;
+}
+
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return `Seu IMC é de ${imc.toFixed(1)} e você está abaixo do peso.`;
+    } else if(imc >= 18.5 && imc < 25) {
+        return `Seu IMC é de ${imc.toFixed(1)} e você está com o peso normal.`;
+    } else if(imc >= 25 && imc < 30) {
+        return `Seu IMC é de ${imc.toFixed(1)} e você está acima do peso.`;
+    } else if(imc >= 30 && imc < 40) {
+        return `Seu IMC é de ${imc.toFixed(1)} e você está obeso.`;
+    } else {
+        return `Seu IMC é de ${imc.toFixed(1)} e você está com obesidade grave.`;
+    }
+}
+
+function main() {
+    const peso = 80;
+    const altura = 1.80;
+
+    const imc = calcularImc(peso, altura);
+
+    console.log(classificarImc(imc));
+}
+
+main();
