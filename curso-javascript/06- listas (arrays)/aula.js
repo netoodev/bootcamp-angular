d2626f47980217157570cf0fae9a9f9cfdcba2f1
Neto <netoodeveloper@gmail.com>
2023-12-28 02:32:14 -0300

    Executando For Para Exibir a Média de Notas

--- curso-javascript/06- listas (arrays)/aula.js
+++ curso-javascript/06- listas (arrays)/aula.js
@@ -1,11 +1,17 @@
 const notas = [];
 
 notas.push(5);
-notas.push(7);
-notas.push(8);
-notas.push(2);
-notas.push(5);
+notas.push(10);
+notas.push(10);
 
-const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
 
-console.log(soma / notas.length);
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+}
+
+const media = soma / notas.length;
+
+console.log(media.toFixed(1));
