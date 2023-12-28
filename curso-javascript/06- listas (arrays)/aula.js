6ecef88c73b764dda9dfdeb91dfd04662c92b49c
Neto <netoodeveloper@gmail.com>
2023-12-28 02:23:15 -0300

    Entendendo a Necessidade de Estruturas de Repetição

--- curso-javascript/06- listas (arrays)/aula.js
+++ curso-javascript/06- listas (arrays)/aula.js
@@ -1,9 +1,11 @@
-const alunos = [];
+const notas = [];
 
-alunos.push('teste');
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
 
-alunos.push(10);
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
 
-alunos.pop();
-
-console.log(alunos);
+console.log(soma / notas.length);
