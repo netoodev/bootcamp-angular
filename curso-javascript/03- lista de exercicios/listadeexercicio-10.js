a29aae8a5290e364e3c69f74a2198af6722866dd
Neto <netoodeveloper@gmail.com>
2023-12-28 02:49:34 -0300

    Exercício Guiado - Apenas Números Pares

---
+++ curso-javascript/03- lista de exercicios/listadeexercicio-10.js
@@ -0,0 +1,8 @@
+const numeros = [12, 45, 36, 93, 28, 22, 378, 237, 4, 34, 190];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
