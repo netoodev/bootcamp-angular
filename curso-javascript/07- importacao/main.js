dce89cbf7bb8d03a5a17a9da209f4691f30df6b5
Neto <netoodeveloper@gmail.com>
2023-12-28 14:14:51 -0300

    Resolução Guiada do Exercício Maior Número Sorteado

--- curso-javascript/07- importacao/main.js
+++ curso-javascript/07- importacao/main.js
@@ -2,5 +2,20 @@
 
 const {gets, print} = require('./funcoes-auxiliares.js');
 
+const numerosSorteados = [];
 
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
 
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado
+    }
+}
+
+print(maiorValor);
