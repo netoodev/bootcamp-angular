31d362a13a81b1053c750aaef7283b1a929aa02f
Neto <netoodeveloper@gmail.com>
2023-12-28 14:18:21 -0300

    Simplificando a Lógica

--- curso-javascript/07- importacao/main.js
+++ curso-javascript/07- importacao/main.js
@@ -2,20 +2,15 @@
 
 const {gets, print} = require('./funcoes-auxiliares.js');
 
-const numerosSorteados = [];
+let maiorValor = 0;
 
 for (let i = 0; i < 5; i++) {
     const numeroSorteado = gets();
-    numerosSorteados.push(numeroSorteado);
-}
-
-let maiorValor = 0;
-
-for (let i = 0; i < numerosSorteados.length; i++) {
-    const numeroSorteado = numerosSorteados[i];
     if (numeroSorteado > maiorValor) {
         maiorValor = numeroSorteado
     }
 }
+
+
 
 print(maiorValor);
