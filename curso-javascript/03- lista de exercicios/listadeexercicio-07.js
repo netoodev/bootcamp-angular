2cae81e51868799ea919ab3811e5b7a073091394
Neto <netoodeveloper@gmail.com>
2023-12-28 00:08:25 -0300

    Exercício Guiado - Criando a Classe Carro

---
+++ curso-javascript/03- lista de exercicios/listadeexercicio-07.js
@@ -0,0 +1,13 @@
+class Carro {
+    marca;
+    cor;
+    gastoMedioPorKm;
+
+    constructor(marca, cor, gastoMedioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMedioPorKm = gastoMedioPorKm;
+    }
+
+    
+}
