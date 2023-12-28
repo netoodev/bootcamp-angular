a857b0d1155799cb8fee788016e5c0f44632ce46
Neto <netoodeveloper@gmail.com>
2023-12-27 23:08:08 -0300

    Criando Métodos Para o Objeto

--- curso-javascript/05- objetos/aula.js
+++ curso-javascript/05- objetos/aula.js
@@ -1,10 +1,9 @@
-const neto = {
+const pessoa = {
     nome: 'José S N Neto',
     idade: 19,
+    descrever: function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
+    }
 };
 
-neto.altura = 180;
-
-delete neto.nome;
-
-console.log(neto);
+pessoa.descrever();
