0d032d85dae0d0aa1f1cee3a88eb4339e2fe6689
Neto <netoodeveloper@gmail.com>
2023-12-27 23:23:01 -0300

    Como Criar Classes e Instâncias

--- curso-javascript/05- objetos/aula.js
+++ curso-javascript/05- objetos/aula.js
@@ -1,10 +1,20 @@
-const pessoa = {
-    nome: 'José S N Neto',
-    idade: 19,
-    descrever: function () {
-        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
+class Pessoa {
+    nome;
+    idade;
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
-};
+}
 
+const neto = new Pessoa();
+neto.nome = 'José S N Neto';
+neto.idade = 19;
 
-console.log(pessoa['nome']);
+const joao = new Pessoa();
+joao.nome = 'João C S Filho';
+joao.idade = 18
+
+neto.descrever();
+joao.descrever();
+
