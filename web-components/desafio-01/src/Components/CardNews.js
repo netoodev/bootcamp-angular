3d9cfb466b5ad25a99af4cf4c23ed062a631cb55
Neto <netoodeveloper@gmail.com>
2024-01-17 18:03:49 -0300

    Construindo um Component de maneira profissional

--- web-components/desafio-01/src/Components/CardNews.js
+++ web-components/desafio-01/src/Components/CardNews.js
@@ -1,10 +1,18 @@
-class CardNews extends HTMLElement {
+class Cardnews extends HTMLElement {
     constructor(){
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World!</h1>";
+        shadow.innerHTML = '<h1>Hello</h1>'
+    }
+
+    build(){
+
+    }
+
+    styles(){
+        
     }
 }
 
-customElements.define('card-news',CardNews);
+customElements.define("card-news", Cardnews);
