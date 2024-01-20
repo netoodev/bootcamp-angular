f2040330f43d4d5d88ebb2041dba7ddae3a7f7b6
Neto <netoodeveloper@gmail.com>
2024-01-20 07:48:54 -0300

    Atrribute Decorator

--- typescript-para-angular/src/index.ts
+++ typescript-para-angular/src/index.ts
@@ -199,9 +199,41 @@ function apiVersion(version:string){
     }
 }
 
-@apiVersion("1.10")
-class Api {}
+// @apiVersion("1.10")
+// class Api {}
 
-const api = new Api();
+// const api = new Api();
 
-console.log(api.__version)
+//Attribute Decorator
+
+function minLength(length:number) {
+    return (target:any, key:string) => {
+        let _value = target[key];
+
+        const getter = () => _value;
+        const setter = (value:string) => {
+            if (value.length < length) {
+                throw new Error(`Tamanho menor do que ${length} caracteres.`);
+            } else {
+                _value = value;
+            }
+        };
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        });
+    };
+}
+
+class Api {
+    @minLength(3)
+    name:string;
+
+    constructor(name:string){
+        this.name = name;
+    }
+}
+
+const api = new Api("produtos");
+console.log(api.name);
