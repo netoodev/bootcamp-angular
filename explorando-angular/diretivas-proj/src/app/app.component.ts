b2e0e28a5d76c49fb358e28c498a9bda5e9fbbda
Neto <netoodeveloper@gmail.com>
2024-01-29 10:11:09 -0300

    ngIf com ngTemplate

--- explorando-angular/diretivas-proj/src/app/app.component.ts
+++ explorando-angular/diretivas-proj/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CommonModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
