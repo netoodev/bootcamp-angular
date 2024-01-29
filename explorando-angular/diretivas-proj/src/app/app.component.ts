425ee1fd70c9ebfaa11b5243ec15baf69e7c353b
Neto <netoodeveloper@gmail.com>
2024-01-29 11:33:11 -0300

    NgClass

--- explorando-angular/diretivas-proj/src/app/app.component.ts
+++ explorando-angular/diretivas-proj/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CardComponent} from './card/card.component';
+import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CardComponent],
+  imports: [RouterOutlet, CommonModule, CardComponent, CompAtributosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ explorando-angular/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-comp-atributos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './comp-atributos.component.html',
+  styleUrl: './comp-atributos.component.css'
+})
+export class CompAtributosComponent implements OnInit{
+
+  estilo:string = "enable";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  trocar(){
+    if (this.estilo === "disable") {
+      this.estilo = "enable"
+    } else {
+      this.estilo = "disable"
+    }
+  }
+
+}
