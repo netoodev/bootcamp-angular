24161f655350199a9cf5bfaa45dd53ee7c6e9917
Neto <netoodeveloper@gmail.com>
2024-02-16 14:36:28 -0300

    Service conversando com environment

--- explorando-angular/service/src/app/services/pokemon.service.ts
+++ explorando-angular/service/src/app/services/pokemon.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private baseUrl:string = '';
+
+  constructor() {
+    this.baseUrl = environment.pokeApi;
+  }
 
   getPokemon(pokemonName:string) {
     console.log(pokemonName)
